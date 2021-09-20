1300620c020fb6120c4cba699e5559819c168a43
Dondoo23 <junsu5713@naver.com>
2021-09-20 17:52:09 +0900

    [ADD] 회원가입 유효성 검사 + 정규식

---
+++ login/js/signup.js
@@ -0,0 +1,25 @@
+$("#signUpBtn").off("click").on("click", function() {
+    signUpCheck();
+});
+
+function signUpCheck() {
+    var id = $("#id").val();
+    var pw = $("#pw").val();
+    var pw2 = $("#pw2").val();
+    var name = $("#name").val();
+
+    var idCheck = /^[a-zA-Z0-9]{8,16}$/; // 영어 숫자만 사용해서 8~16
+    var pwCheck = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/; // 영어 숫자 특수문자 사용해서 8~16
+    if (!idCheck.test(id)) {
+        alert("8~16자리의 영문자와 숫자를 사용해서 아이디를 입력하세요.");
+    }
+    else if (!pwCheck.test(pw)) {
+        alert("8~16자리의 영문자와 숫자, 특수문자를 사용해서 비밀번호를 입력하세요.");
+    }
+    else if (pw != pw2) {
+        alert("비밀번호를 다시 확인해주세요.");
+    }
+    else if (id == "" || pw == "" || pw2 == "" || name == "") {
+        alert("모든 정보를 입력해주세요.");
+    }
+}
