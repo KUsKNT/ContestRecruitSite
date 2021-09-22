90e068e59d9a7fa598419208a410f88827cbf517
DahnKo <rhrudtn0314@gmail.com>
2021-09-22 20:57:23 +0900

    [UPDATE] js 데이터 코드 작성(test)

--- user/js/addPost.js
+++ user/js/addPost.js
@@ -9,7 +9,7 @@ router.get('/write', function(req,res,next){ // post로 /write로 들어올 때 
     var sql = "insert into board(name, title, content)"
     conn.query(sql, datas, function(err, rows){
         if(err) console.error("err : " + err);
-        res.redirect('/board/list');
+        res.redirect('/board/list'); //
     })
 
 
